68c807433ca686108b3f23558d4afe3408d37b78
Gonzairibarne <gonzaloiribarne@hotmail.com>
2022-07-05 23:13:00 -0300

    first commit

---
+++ mi-archivo.js
@@ -0,0 +1,15 @@
+//  PROMPT extrocura
+
+// let nombre = prompt("Ingrese su usuario");
+// let saludo = "Hola" + nombre;
+// alert(saludo);
+let nombre = prompt("ingrese un nombre");
+let edad = parseInt(prompt("ingrese su edad"));
+
+if(nombre === "andres" && edad <= 18){
+  alert("bienvenido andres")
+
+}else{
+  alert("raja de aca.")
+};
+
